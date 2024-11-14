4a26b197fc8186a898b1ac3ee0e93a1ad41b54fc
Mornark <monarchofaether@gmail.com>
2024-11-14 06:53:18 -0500

    Basic Auth class defined, inherits from Auth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,8 +16,12 @@ auth = None
 
 auth = getenv('AUTH_TYPE')
 if auth:
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+    if auth == 'auth':
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+    else:
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Defines the BasicAuth class that inherits from the Auth Class
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    pass
