aea27dac38b45a82c27d4a19f308700be94dbe12
Mornark <monarchofaether@gmail.com>
2024-11-14 06:54:32 -0500

    Doc added for class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-""" Defines the BasicAuth class that inherits from the Auth Class
+""" The BasicAuth class that inherits from the Auth Class
 """
 from .auth import Auth
 
 
 class BasicAuth(Auth):
+    """ Defines the BasicAuth Class
+    """
     pass
