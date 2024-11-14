0c9bac8d53b2263af2945c6b21e847596b1397ac
Mornark <monarchofaether@gmail.com>
2024-11-14 09:31:07 -0500

    Decodes authentication token in request header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,9 +2,34 @@
 """ The BasicAuth class that inherits from the Auth Class
 """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """ Defines the BasicAuth Class
     """
-    pass
+    def extract_base64_authorization_header(
+                                            self, authorization_header: str
+                                            ) -> str:
+        """ Extracts the token from the authorization header
+        """
+        if (authorization_header is None
+                or not isinstance(authorization_header, str)
+                or not authorization_header.startswith('Basic ')):
+            return None
+        return authorization_header[len('Basic '):]
+
+    def decode_base64_authorization_header(
+                                           self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ Decodes the token from the authorization header
+        """
+        if (base64_authorization_header is None
+                or not isinstance(base64_authorization_header, str)):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
