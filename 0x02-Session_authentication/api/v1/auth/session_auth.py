9d8a1bd2fd9a30dd97678269dcd79f359c965f65
Mornark <monarchofaether@gmail.com>
2024-11-15 11:40:19 -0500

    Changing import back to relative import

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ The Session Authentication Class for Session authentication
 """
-from api.v1.auth.auth import Auth
+from .auth import Auth
 from os import getenv
 import uuid
 
